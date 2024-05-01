cd6b2c8be2172b8af2845c9a3e16b1dd4209f1b7
Chandrashekhar <shekharsuthar234@gmail.com>
2024-05-01 13:02:35 +0530

    Initial commit

---
+++ lib/practice.dart
@@ -0,0 +1,17 @@
+void main() {
+  /*
+  * tHIS IS A dARD COMMENT
+  * THIS IS NEW DART COMMENT
+  * HEY DARTY COMMENT IS HERE
+  * HOW ARE YOU DART COMMENT
+  * THIS IS NOT RECOMMENDED TYPES OF COMMENT
+  */
+
+
+  // Variables
+
+  // <data Type> <variable Name> = <value>;
+
+
+
+}
